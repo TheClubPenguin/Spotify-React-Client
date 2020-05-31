412dd22c4c083b2bcbdd1fe77ef6bb3a01122019
Chris Stanley <CStanley4848@gmail.com>
2020-05-30 17:36:36 -0700

    Got the client recognizing the server socket

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,19 +1,31 @@
 import React, {Component} from 'react'
-const io = require('socket.io-client')
+import socketIOClient from "socket.io-client"
 const ENDPOINT = "http://127.0.0.1:8000";
+
 
 class Chat extends Component{
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      message: "Test",
+    };
   };
 
-  socket = io(ENDPOINT);
+  componentDidMount () {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on('connect', () => {
+      console.log('connect')
+    })
+  }
+
 
 
   render () {
     return (
-      <h1>Test</h1>
+    <div>
+      <h1>{this.state.message}</h1>
+      <button id='pressMe'>Press Me</button>
+    </div>
     );
 
   };
