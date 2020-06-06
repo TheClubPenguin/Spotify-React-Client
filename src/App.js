d6d929f57e7a55e710a07e11bd663b4081e4d64b
Chris Stanley <CStanley4848@gmail.com>
2020-06-06 12:55:39 -0700

    Information typed into a browser window is now transmittd all conncected pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
-import Chat from './components/Chat'
+import Chat from './components/chat'
 import './App.css';
 
 function App() {
